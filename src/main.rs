9d04215bffd7d2d4989c22880847aa8a597eee3d
William Santasiero <wsantas@live.com>
2020-05-10 19:51:20 -0400

    Readd gmail1 code

--- src/main.rs
+++ src/main.rs
@@ -10,6 +10,16 @@
 mod task;
 #[cfg(test)] mod tests;
 
+extern crate hyper;
+extern crate hyper_rustls;
+extern crate yup_oauth2 as oauth2;
+extern crate google_gmail1 as gmail1;
+use gmail1::{Error, Scope};
+use std::default::Default;
+use oauth2::{Authenticator, DefaultAuthenticatorDelegate, ApplicationSecret, MemoryStorage, FlowType};
+use gmail1::Gmail;
+use std::env;
+
 use rocket::Rocket;
 use rocket::fairing::AdHoc;
 use rocket::request::{Form, FlashMessage};
@@ -78,6 +88,52 @@ fn index(msg: Option<FlashMessage>, conn: DbConn) -> Template {
     })
 }
 
+#[get("/")]
+fn hello(msg: Option<FlashMessage>, conn: DbConn) -> Template {
+    Template::render("hello", &match msg {
+        Some(ref msg) => Context::raw(&conn, Some((msg.name(), msg.msg()))),
+        None => Context::raw(&conn, None),
+    })
+}
+
+fn gmail_connector_test() {
+    println!("Starting rusty");
+
+    let key = env::var("google_secret").unwrap();
+    let client_id = env::var("google_client_id").unwrap();
+    let mut secret: ApplicationSecret = Default::default();
+    secret.client_secret = key;
+    secret.client_id = client_id;
+
+    let auth = Authenticator::new(&secret, DefaultAuthenticatorDelegate,
+                                  hyper::Client::with_connector(hyper::net::HttpsConnector::new(hyper_rustls::TlsClient::new())),
+                                  <MemoryStorage as Default>::default(), None);
+    let hub = Gmail::new(hyper::Client::with_connector(hyper::net::HttpsConnector::new(hyper_rustls::TlsClient::new())), auth);
+
+    println!("Starting rusty3");
+    let result = hub.users()
+        .messages_get("wsantas@gmail.com", "<398dc2a3d54f4413817308ca57778a34@prdphjobs01>").add_scope(Scope::Readonly)
+        .doit();
+    println!("Starting rusty4");
+    match result {
+        Err(e) => match e {
+            // The Error enum provides details about what exactly happened.
+            // You can also just use its `Debug`, `Display` or `Error` traits
+            Error::HttpError(_)
+            | Error::MissingAPIKey
+            | Error::MissingToken(_)
+            | Error::Cancelled
+            | Error::UploadSizeLimitExceeded(_, _)
+            | Error::Failure(_)
+            | Error::BadRequest(_)
+            | Error::FieldClash(_)
+            | Error::JsonDecodeError(_, _) => println!("{}", e),
+        },
+        Ok(res) => println!("Success: {:?}", res)
+
+    }
+}
+
 fn run_db_migrations(rocket: Rocket) -> Result<Rocket, Rocket> {
     let conn = DbConn::get_one(&rocket).expect("database connection");
     match embedded_migrations::run(&*conn) {
@@ -96,6 +152,7 @@ fn rocket() -> Rocket {
         .mount("/", StaticFiles::from("static/"))
         .mount("/", routes![index])
         .mount("/todo", routes![new, toggle, delete])
+        .mount("/hello", routes![hello])
         .attach(Template::fairing())
 }
 
